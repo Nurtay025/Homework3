6a5d6e68ddd570e5c29903e2aa101dae5d322a59
Nurtay025 <nurtayabdyraev@gmail.com>
2019-07-27 22:20:08 +0600

    Homework 3

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+/**
+ * Homework 3
+ *
+ * @author "Tai"
+ */
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        String names[] = new String[3];
+        names[0] = "Nurbek";
+        names[1] = "Nurkai";
+        names[2] = "Nurtai";
+
+        for (String n : names) {
+
+
+            switch (n) {
+                case "Nurbek":
+                    System.out.println("Good morning" + " Nurbek");
+                    break;
+                case "Nurkai":
+                    System.out.println("Good afternoon" + " Nurkai");
+                    break;
+                case "Nurtai":
+                    System.out.println("Good evening" + " Nurtai");
+
+            }
+        }
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
